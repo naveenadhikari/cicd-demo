58a215827b8207608a97449779563693dbf367d7
naveenadhikari <95695435+naveenadhikari@users.noreply.github.com>
2026-03-22 11:29:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hey there this is august ', status: 'ok' });
+  res.json({ message: 'Hey there this is august ! ', status: 'ok' });
 });
 
 app.get('/health', (req, res) => {
